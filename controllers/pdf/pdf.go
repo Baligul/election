8d5cc0e89822a0279d64e42ba24104bb04a9e70d
Baligul Hasan <baligul.hasan@dynomitedb.com>
2016-11-20 16:56:40 +0530

    test pdf

--- controllers/pdf/pdf.go
+++ controllers/pdf/pdf.go
@@ -154,7 +154,7 @@ func (e *PdfCtrl) CreateAndSendPdf() {
 		pdf.CellFormat(wSum, 0, "", "T", 0, "", false, 0, "")
 	}
 	fancyTable()
-	e.Data["json"] = pdf
+	//e.Data["json"] = pdf
 	err = pdf.OutputFileAndClose("hello.pdf")
 	if err != nil {
 		responseStatus := modelVoters.NewResponseStatus()
@@ -169,7 +169,7 @@ func (e *PdfCtrl) CreateAndSendPdf() {
 	//responseStatus.Response = "ok"
 	//responseStatus.Message = fmt.Sprintf("The file has been sent Successfully.")
 	//e.Data["json"] = &responseStatus
-	e.ServeJSON()
+	return pdf
 	//example.Summary(err, fileName)
 	// Output:
 	// Successfully generated pdf/Fpdf_CellFormat_tables.pdf
