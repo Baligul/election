a80c7ab422f4bb178a7be81a03a7da0eb9b48796
root <root@eubda.c.adept-bison-151408.internal>
2017-01-05 16:55:19 +0530

    Delete unnecessary files

--- others.sql
+++
@@ -1,3 +0,0 @@
-UPDATE voter_19 SET religion_english = 'Others', religion_hindi = 'अन्य' WHERE name_english IS NULL;
-UPDATE voter_20 SET religion_english = 'Others', religion_hindi = 'अन्य' WHERE name_english IS NULL;
-UPDATE voter_21 SET religion_english = 'Others', religion_hindi = 'अन्य' WHERE name_english IS NULL;
